08243c09b90421f8de9976db1adb1a52ff13aa3c
Joey Sandine & Tina Ramsey <student@epicodus.com>
2015-01-13 16:34:56 -0800

    be a digital pet you can kill

---
+++ lib/toma.rb
@@ -0,0 +1,41 @@
+class Tamagotchi
+  define_method(:initialize) do |name|
+    @name = name
+    @food_level = 10
+    @sleep_level = 10
+    @activity_level = 10
+  end
+
+  define_method(:name) do
+    @name
+  end
+
+  define_method(:food_level) do
+    @food_level
+  end
+
+  define_method(:sleep_level) do
+    @sleep_level
+  end
+
+  define_method(:activity_level) do
+    @activity_level
+  end
+
+  define_method(:time_passes) do |time_units|
+    time_units.times() do
+      @food_level = @food_level.-(1)
+      @activity_level = @activity_level.-(1)
+      @sleep_level = @sleep_level.-(1)
+    end
+  end
+
+  define_method(:is_alive?) do
+    if @food_level==0
+      false
+
+    else
+      true
+    end
+  end
+end
---
+++ spec/toma_spec.rb
@@ -0,0 +1,46 @@
+require('rspec')
+require('toma')
+require('pry')
+
+describe("Tamagotchi") do
+  describe("#initialize") do
+    it("sets the name and a few other properties") do
+      my_pet = Tamagotchi.new("Digital Friend")
+      expect(my_pet.name()).to(eq("Digital Friend"))
+      expect(my_pet.food_level()).to(eq(10))
+      expect(my_pet.sleep_level()).to(eq(10))
+      expect(my_pet.activity_level()).to(eq(10))
+    end
+  end
+
+  describe("#time_passes") do
+    it("decreases the amount of food the Tamagotchi has left by 1") do
+      my_pet = Tamagotchi.new("Digital Friend")
+      my_pet.time_passes(1)
+      expect(my_pet.food_level()).to(eq(9))
+    end
+    it("decreases the amount of activity the Tamagotchi has left by 1") do
+      my_pet = Tamagotchi.new("Digital Friend")
+      my_pet.time_passes(1)
+      expect(my_pet.activity_level()).to(eq(9))
+    end
+    it("decreases the amount of sleep the Tamagotchi has left by 1") do
+      my_pet = Tamagotchi.new("Digital Friend")
+      my_pet.time_passes(1)
+      expect(my_pet.sleep_level()).to(eq(9))
+    end
+  end
+
+  describe("#is_alive?") do
+    it("is alive if the food level is above 0") do
+      my_pet = Tamagotchi.new("Digital Friend")
+      expect(my_pet.is_alive?()).to(eq(true))
+    end
+
+    it("is dead if the food level is 0") do
+      my_pet = Tamagotchi.new("Digital Friend")
+      my_pet.time_passes(10)
+      expect(my_pet.is_alive?()).to(eq(false))
+    end
+  end
+end
